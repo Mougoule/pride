f703d8087d1d978558a7a621fa131856665ea3c0
Mougoule <ouldbouamama.nicolas@gmail.com>
2015-03-10 15:37:34 +0100

    paramétrage

--- pride/src/test/java/fr/hardis/client/CustomerBusinessServiceTest.java
+++
@@ -1,9 +0,0 @@
-package fr.hardis.client;
-
-//import org.apache.cxf.jaxrs.client.WebClient;
-//import org.junit.BeforeClass;
-//import org.junit.Test;
-
-public class CustomerBusinessServiceTest {
-
-}
